a721d86b225b1130ba6883f635e45946c9609d64
tcarisland <tcarisland@gmail.com>
2024-02-07 14:34:36 +0100

    spring project initialized

---
+++ src/main/java/com/tcarisland/tclang/TclangApplication.java
@@ -0,0 +1,13 @@
+package com.tcarisland.tclang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TclangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TclangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tcarisland/tclang/TclangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcarisland.tclang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TclangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
