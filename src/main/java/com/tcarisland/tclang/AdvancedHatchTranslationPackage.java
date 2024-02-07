0b7aff29d780ccfbdf72acabaad11ac1b60c8685
tcarisland <tcarisland@gmail.com>
2024-02-07 20:32:26 +0100

    yml parsing seems to work

---
+++ src/main/java/com/tcarisland/tclang/AdvancedHatchTranslationPackage.java
@@ -0,0 +1,9 @@
+package com.tcarisland.tclang;
+
+import lombok.Data;
+
+import java.util.List;
+@Data
+public class AdvancedHatchTranslationPackage {
+    List<AdvancedHatchItemLabel> labels;
+}
--- src/main/java/com/tcarisland/tclang/TclangApplication.java
+++ src/main/java/com/tcarisland/tclang/TclangApplication.java
@@ -1,7 +1,13 @@
 package com.tcarisland.tclang;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 @SpringBootApplication
 public class TclangApplication {
@@ -9,5 +15,22 @@ public class TclangApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TclangApplication.class, args);
 	}
+
+	public TclangApplication() {
+		ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        try {
+            AdvancedHatchTranslationPackage advancedHatchTranslationPackage = mapper
+					.readValue(
+							new File("src/main/resources/projects/advancedhatch/advancedhatch.yml"),
+							AdvancedHatchTranslationPackage.class
+					);
+			advancedHatchTranslationPackage.getLabels().forEach(u -> {
+				System.out.printf("%s %s %n", u.getDestination(), u.getName());
+			});
+        } catch (IOException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+        }
+    }
 
 }
